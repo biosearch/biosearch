619fc3c356c7a6be9695100779fe858327ba3b9d
digitallance <raf.podowski@gmail.com>
2019-01-28 16:31:59 -0800

    ability to get both baseline and/or update files in one go

--- biosearch/pubmed/collect.py
+++ biosearch/pubmed/collect.py
@@ -6,6 +6,8 @@ Content collection module.
 
 import ftputil
 import gzip
+import os
+import sys
 from io import BytesIO
 import urllib.request
 
@@ -17,21 +19,27 @@ class Downloader:
     # Location of PubMed files
     repo = {"baseline": "/pubmed/baseline/", "updates": "/pubmed/updatefiles/"}
 
-    def __init__(self, repository):
+    def __init__(self, repository=None):
         """
         Args:
-            repository (str) - type of PubMed XML files to process:
+            repository (str) - [optional] type of PubMed XML files to process:
                 'baseline' or 'updates'
         """
-        self.src = self.repo[repository]
+        if repository and repository in self.repo.keys():
+            self.sources = [repository]
+        else:
+            self.sources = list(self.repo.keys())
+        print(self.sources)
         # connect to FTP server
+        self.file_list = list()
         with ftputil.FTPHost("ftp.ncbi.nlm.nih.gov", "anonymous", "") as ftp_host:
-            # get list of files in repository
             ftp_host.use_list_a_option = False
-            ftp_host.chdir(self.src)
-            self.file_list = [
-                fn for fn in ftp_host.listdir(ftp_host.curdir) if fn.endswith("xml.gz")
-            ]
+            # get list of files in repository
+            for src in self.sources:
+                ftp_host.chdir(self.repo[src])
+                for fn in ftp_host.listdir(ftp_host.curdir):
+                    if fn.endswith("xml.gz"):
+                        self.file_list.append(src + "/" + fn)
 
     def xml_file_generator(self, filter=None):
         """
@@ -44,14 +52,17 @@ class Downloader:
         """
         filter[0] = max(1, filter[0])
 
-        for fname in self.file_list:
+        for fpath in self.file_list:
 
             # file number
+            fname = os.path.basename(fpath)
+            # print(">>>", fpath, "::", fname)
             this_file_number = int(fname[9:13])
             if filter and not filter[0] <= this_file_number <= filter[1]:
                 continue
 
-            url = "ftp://ftp.ncbi.nlm.nih.gov/%s/%s" % (self.src, fname)
+            url = "ftp://ftp.ncbi.nlm.nih.gov/pubmed/%s" % (fpath)
+            # print(url)
             mysock = urllib.request.urlopen(url)
             memfile = BytesIO(mysock.read())
             yield gzip.GzipFile(fileobj=memfile).read().decode("utf-8")
@@ -59,15 +70,16 @@ class Downloader:
 
 if __name__ == "__main__":
     # Run some tests
-    D = Downloader("baseline")
-    filter = [960, 970]
-    for fname in D.file_list:
+    D = Downloader()
+    filter = [970, 1001]
+    for fpath in D.file_list:
+        fname = os.path.basename(fpath)
         this_file_number = int(fname[9:13])
         # print(this_file_number)
         if filter and not filter[0] <= this_file_number <= filter[1]:
             continue
 
-        print(fname)
+        print(fpath)
 
     for xml_file in D.xml_file_generator([972, 972]):
         print(xml_file[:1000])
